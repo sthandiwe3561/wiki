ecfaaff4db9f15deef82d6f35c5f6c86ac86b47e
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2025-01-08 11:40:10 +0200

    add search.html and entry_page.html

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>/", views.entry_page, name="entry_page"),
+    path("search/", views.search, name="search")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+def entry_page(request,title):
+    #fecthing the content of the entry
+    content = util.get_entry(title)
+
+    return render(request,"encyclopedia/entry_page.html", {
+        "content":content , "title": title.capitalize()
+    })
+
+def search(request):
+    if request.method == "POST":
+        value = request.POST.get("q", "")
+        if value:
+            content = util.get_entry(value)
+
+            if content:
+               return render("encyclopedia/entry_page",{
+                "content":content
+               })
+            else:
+              lower_value = value.lower()
+              entries = util.list_entries()
+              partial_matches = [entry for entry in entries if lower_value in entry.lower()]
+              return render(request,"encyclopedia/search_page.html", {
+                  "results": partial_matches, "query":value
+              })
+
+       
+    else:
+        return redirect(reverse("index"))
 
